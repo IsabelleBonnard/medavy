2d4f30b52608b18d3a9e2ccee6ddeeb75b508a90
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-04-05 10:41:19 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  get 'works', to: 'pages#works'
+  get 'history', to: 'pages#history'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
