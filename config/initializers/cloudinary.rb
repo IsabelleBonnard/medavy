da6965d11a5d0074bd1bff3a0f79d5332adb46ad
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-04-06 17:11:32 +0200

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+# Cloudinary.config do |config|
+#    config.cloud_name = 'dsosadxbk'
+#    config.api_key = '528713975184588'
+#    config.api_secret = 'Vjrc1wxpH8JrGX2X_bW9C3Zjrv4'
+#    config.cdn_subdomain = true
+#  end
