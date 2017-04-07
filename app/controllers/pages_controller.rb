4f99403d3bb973a17dcff7e275b46a4f700a966f
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-04-07 12:30:07 +0200

    photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :contact, :about, :history, :gallery, :exterior, :interiors, :medavy_by_night, :sky_views, :work, :send_contact_message]
 
@@ -14,18 +17,44 @@ class PagesController < ApplicationController
   end
 
   def exterior
+    # all = Cloudinary::Api.resources
+    # results = Cloudinary::Api.resources(type:"upload")
+    # resources = results["resources"]
+    # ids = resources.map {|res| res["public_id"]}
+    # @ids = ids
+    # ids.select! {|public_id| !public_id.include?("/")}
+    url = "http://res.cloudinary.com/dsosadxbk/image/list/exterior.json"
+    my_json = JSON.parse(open(url).read)
+    resources = my_json["resources"]
+    @ids = resources.map {|res| res["public_id"]}
   end
 
   def interiors
+    url = "http://res.cloudinary.com/dsosadxbk/image/list/interiors.json"
+    my_json = JSON.parse(open(url).read)
+    resources = my_json["resources"]
+    @ids = resources.map {|res| res["public_id"]}
   end
 
   def medavy_by_night
+    url = "http://res.cloudinary.com/dsosadxbk/image/list/work.json"
+    my_json = JSON.parse(open(url).read)
+    resources = my_json["resources"]
+    @ids = resources.map {|res| res["public_id"]}
   end
 
   def sky_views
+    url = "http://res.cloudinary.com/dsosadxbk/image/list/sky.json"
+    my_json = JSON.parse(open(url).read)
+    resources = my_json["resources"]
+    @ids = resources.map {|res| res["public_id"]}
   end
 
   def work
+    url = "http://res.cloudinary.com/dsosadxbk/image/list/night.json"
+    my_json = JSON.parse(open(url).read)
+    resources = my_json["resources"]
+    @ids = resources.map {|res| res["public_id"]}
   end
 
 
