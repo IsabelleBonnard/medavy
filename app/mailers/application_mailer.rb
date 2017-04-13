cfb2e16877a0dfe53f80cb14199397cb6c25cd30
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-04-13 16:49:39 +0200

    app password

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'isabelle.charon@gmail.com'
+  default from: 'contact.medavy@gmail.com'
   layout 'mailer'
   add_template_helper(EmailHelper)
 end
--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -3,6 +3,6 @@ class MessagesMailer < ApplicationMailer
     @name = message.name
     @email = message.email
     @content = message.content
-    mail(to: "isabelle.charon@gmail.com", subject: t('messages.subject', name: @name))
+    mail(to: "contact.medavy@gmail.com", subject: t('messages.subject', name: @name))
   end
 end
