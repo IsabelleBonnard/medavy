0d1a2f5f822c40ef8ede4005513067e4fd7c9928
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-05-04 10:31:22 +0200

    test config assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
   Rails.application.config.assets.paths << path
 end
 
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "Exterior")
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
