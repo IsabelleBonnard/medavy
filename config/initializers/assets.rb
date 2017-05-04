c517538e18c38c4fe294f1ef7929944ad40fddb3
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-05-04 10:57:10 +0200

    test 3

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,8 +11,8 @@ Rails.application.config.assets.version = '1.0'
 #   Rails.application.config.assets.paths << path
 # end
 
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "Exterior")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "Exterior")
-
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
