aeab23648eb6919fab33c44a04ba6d106d6dc576
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-05-10 18:48:56 +0200

    test 2 contact@zadl.fr

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -3,6 +3,6 @@ class MessagesMailer < ApplicationMailer
     @name = message.name
     @email = message.email
     @content = message.content
-    mail(to: "contact.medavy@gmail.com", subject: t('messages.subject', name: @name))
+    mail(to: "contact@zadl.fr", subject: t('messages.subject', name: @name))
   end
 end
