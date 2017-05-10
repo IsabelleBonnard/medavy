ad10b764701f889dafec63c310a2adc95b58aa72
Isabelle Bonnard <isabelle.charon@gmail.com>
2017-05-10 09:39:24 +0200

    mails from contact.medavy@zadl.fr

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'contact.medavy@gmail.com'
+  default from: 'contact.medavy@zadl.fr'
   layout 'mailer'
   add_template_helper(EmailHelper)
 end
